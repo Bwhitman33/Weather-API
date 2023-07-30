6bd7ff0062d1039cd9ae7134933e4c5243fc0e06
Bwhitman33 <bwhitman33@gmail.com>
2023-07-30 19:34:37 -0400

    Search values now display in a section underneath the searchbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,59 @@
 // set api key for global use
-const apiKey = '2e300aff68a4157db51a64418f47cc58';
+var apiKey = "2e300aff68a4157db51a64418f47cc58";
 
-// const searchHistory = [];
+// set global for search history
+var searchHistory = [];
 
 // create event handler to handle search button input functionality
-$('#search-button').on('click', function(e) {
+$("#search-button").on("click", function (e) {
+  e.preventDefault();
+  var citySearched = $("#search-value").val();
+  console.log(citySearched);
+  if (citySearched === "" || citySearched == null) {
+    alert("Please enter the name of a city");
     e.preventDefault();
-    const citySearched = $('#search-value').val();
-    console.log(citySearched)
-
-    if (citySearched === "" || citySearched == null) {
-        alert('Please enter the name of a city');
-        e.preventDefault();
-    } else {
+  } else {
     // use variables here for setting todays weather and forecast after creating functions for them. Coming back to this
+    // localStorage.setItem('citySearched', citySearched)
+    searchLog(citySearched);
+  }
+});
+
+// create functions to define search history, add container to html, and append to container
+var searchLog = function (citySearched) {
+  $('.search-history:contains("' + citySearched + '")').remove();
+  var logEntry = $("<p>");
+  logEntry.addClass("search-history");
+  logEntry.text(citySearched);
+
+  var logEntryContainer = $("<div>");
+  logEntryContainer.addClass("search-history-container");
+  logEntryContainer.append(logEntry);
+
+  var logEntryContainerEl = $("#previous-search-container");
+  logEntryContainerEl.append(logEntryContainer);
+
+  if (searchHistory.length > 0) {
+    var totalSearchHistory = localStorage.getItem("searchHistory");
+    searchHistory = JSON.parse(totalSearchHistory);
+  }
+
+  searchHistory.push(citySearched);
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+
+  $("#search-value").val("");
+};
+
+var loadSearchLog = function () {
+    var storedSearches = localStorage.getItem('searchHistory');
+    if (!storedSearches) {
+        return false;
     }
-})
+
+    storedSearches = JSON.parse(storedSearches);
+    for (var i = 0; i < storedSearches.length; i++) {
+        searchLog(storedSearches[i]);
+    }
+};
+
+loadSearchLog();
